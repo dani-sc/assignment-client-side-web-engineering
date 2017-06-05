51068430a10d9a1fe31df37eceac866de7aa654b
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2017-06-05 23:45:07 +0200

    Add functionality to store-pouchdb implementation

--- 06-db/src/store-pouchdb.js
+++ 06-db/src/store-pouchdb.js
@@ -1,7 +1,21 @@
 import PouchDB from 'pouchdb'
 
 const localDB = new PouchDB('mmt-ss2017')
-const remoteDB = new PouchDB('https://couchdb.5k20.com/mmt-ss2017')
+const remoteDB = new PouchDB('https://couchdb.5k20.com/mmt-ss2017', {
+    auth: {
+        username: 'dscheffknecht',
+        password: 'test',
+    }
+})
+
+localDB
+    .sync(remoteDB, {
+        live: true,
+        retry: true
+    })
+    .on('error', err => {
+        console.error(`An error occured: ${err}`)
+    })
 
 export default class Store {
     /**
@@ -9,25 +23,32 @@ export default class Store {
      * @param {function()} [callback] Called when the Store is ready
      */
     constructor(name, callback) {
-        /**
-         * @type {ItemList}
-         */
-        let liveTodos
 
         /**
-         * Read the local ItemList from localStorage.
+         * Read the local ItemList from localDB.
          *
          * @returns {ItemList} Current array of todos
          */
         this.getStore = () => {
-        }
-
-        /**
-         * Write the local ItemList to localStorage.
-         *
-         * @param {ItemList} todos Array of todos to write
-         */
-        this.setStore = (todos) => {
+            return new Promise(resolve => {
+                localDB.allDocs({
+                    include_docs: true,
+                }).then(docs => {
+                    const todos = docs.rows.map((row) => {
+                        const doc = row.doc
+                        return {
+                            // the TODO-application itself relies on id instead of _id
+                            id: doc._id,
+                            title: doc.title,
+                            completed: doc.completed,
+                            _rev: doc._rev,
+                        }
+                    })
+                    resolve(todos)
+                }).catch(reason => {
+                    console.error(`Failed to fetch all documents, reason: ${reason}`)
+                })
+            })
         }
 
         if (callback) {
@@ -47,7 +68,19 @@ export default class Store {
 	 * })
      */
     find(query, callback) {
-
+        this.getStore().then(todos => {
+            // Original code from store.js:
+            let k
+            const filtered = todos.filter(todo => {
+                for (k in query) {
+                    if (query[k] !== todo[k]) {
+                        return false
+                    }
+                }
+                return true
+            })
+            callback(filtered)
+        })
     }
 
     /**
@@ -57,7 +90,17 @@ export default class Store {
      * @param {function()} [callback] Called when partialRecord is applied
      */
     update(update, callback) {
+        this.getStore().then(todos => {
+            let todoToUpdate = todos.find(todo => todo.id === update.id.toString())
+            Object.assign(todoToUpdate, update)
 
+            localDB.put({
+                _id: todoToUpdate.id.toString(),
+                title: todoToUpdate.title,
+                completed: todoToUpdate.completed,
+                _rev: todoToUpdate._rev
+            }).then(callback())
+        })
     }
 
     /**
@@ -67,7 +110,11 @@ export default class Store {
      * @param {function()} [callback] Called when item is inserted
      */
     insert(item, callback) {
-
+        localDB.put({
+            _id: item.id.toString(),
+            title: item.title,
+            completed: item.completed,
+        }).then(() => callback())
     }
 
     /**
@@ -77,7 +124,35 @@ export default class Store {
      * @param {function(ItemList)|function()} [callback] Called when records matching query are removed
      */
     remove(query, callback) {
+        this.getStore().then(todos => {
+            let todosToKeep = []
+            let todosToDelete = []
 
+            todos.forEach(todo => {
+                for (const k in query) {
+                    // While query[k] is a number, todo[k] is a string. Let's cast.
+                    if (query[k].toString() === todo[k]) {
+                        todosToDelete.push(todo)
+                        return
+                    }
+                }
+                todosToKeep.push(todo)
+            })
+
+            // Let's not use the provided remove method from PouchDB.
+            // Reason: https://pouchdb.com/api.html#filtered-replication
+            todosToDelete = todosToDelete.map(todo => ({
+                _id: todo.id,
+                title: todo.title,
+                completed: todo.completed,
+                _rev: todo._rev,
+                _deleted: true
+            }))
+
+            localDB
+                .bulkDocs(todosToDelete)
+                .then(() => callback(todosToKeep))
+        })
     }
 
     /**
@@ -86,6 +161,15 @@ export default class Store {
      * @param {function(number, number, number)} callback Called when the count is completed
      */
     count(callback) {
-
+        this.getStore().then(todos => {
+            const total = todos.length
+            const completed = todos.reduce((completedCount, curTodo) => {
+                if (curTodo.completed) {
+                    ++completedCount
+                }
+                return completedCount
+            }, 0)
+            callback(total, total - completed, completed)
+        })
     }
 }
